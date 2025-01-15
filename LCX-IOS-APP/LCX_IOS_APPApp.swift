a82de9d9e869d37555ef01b35a605881d6621c74
lcx <forever_river@outlook.com>
2025-01-15 14:01:11 +0800

    Initial Commit

---
+++ LCX-IOS-APP/LCX_IOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCX_IOS_APPApp.swift
+//  LCX-IOS-APP
+//
+//  Created by lcx001st on 2025/1/15.
+//
+
+import SwiftUI
+
+@main
+struct LCX_IOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LCX-IOS-APPTests/LCX_IOS_APPTests.swift
@@ -0,0 +1,17 @@
+//
+//  LCX_IOS_APPTests.swift
+//  LCX-IOS-APPTests
+//
+//  Created by lcx001st on 2025/1/15.
+//
+
+import Testing
+@testable import LCX_IOS_APP
+
+struct LCX_IOS_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
